2c848b5b46a4cbcdeec10348b3301b1f8edfc807
Avani Kale <avani.kale@thoughtworks.com>
2020-04-20 22:21:31 +0530

    [avani]Refactor.  from float datatype to double.

--- src/main/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCart.java
+++ src/main/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCart.java
@@ -1,21 +1,19 @@
 package com.thoughtworks.bootcamp.shoppingCart;
 
-import java.util.ArrayList;
-
 public class ShoppingCart {
     private String apple;
-    private float price;
+    private double price;
     private int quantity;
 
-    public ShoppingCart(String apple, float price, int quantity) {
+    public ShoppingCart(String apple, double price, int quantity) {
         this.apple = apple;
         this.price = price;
         this.quantity = quantity;
     }
 
-    public float totalPrice(float price) {
-        float totalPrice = price * getQuantity();
-        return totalPrice;
+    public double totalPrice(double price) {
+        double totalPrice = price * getQuantity();
+        return  totalPrice;
     }
 
     public int getQuantity() {
--- src/test/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCartTest.java
@@ -3,27 +3,27 @@ package com.thoughtworks.bootcamp.shoppingCart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 
 public class ShoppingCartTest {
 
     @Test
     void givenEmptyShoppingCart_whenAddOneApple_thenShouldReturnOneApplePrice() {
-        ShoppingCart cart = new ShoppingCart("apple", (float) 0.99, 1);
-        float oneApplePrice = (float) 0.99;
+        ShoppingCart cart = new ShoppingCart("apple", 0.99, 1);
+        double oneApplePrice = 0.99;
 
-        Assertions.assertEquals((float) 0.99, cart.totalPrice(oneApplePrice));
+        Assertions.assertEquals(0.99, cart.totalPrice(oneApplePrice));
 
     }
 
     @Test
     void givenShoppingCart_whenAppleQuantityIsTwo_thenShouldReturnPriceOnePointNineEight() {
 
-        ShoppingCart shoppingCart = new ShoppingCart("apple", (float) 0.99, 2);
+        ShoppingCart shoppingCart = new ShoppingCart("apple", 0.99, 2);
 
-        float applePrice = (float) 0.99;
-        Assertions.assertEquals((float)1.98, shoppingCart.totalPrice(applePrice));
+        double applePrice = 0.99;
+        Assertions.assertEquals(1.98, shoppingCart.totalPrice(applePrice));
 
     }
+
 
 }
