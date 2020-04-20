976eaa3452b675b6759fd8f52e36c995f26ed0a4
Avani Kale <avani.kale@thoughtworks.com>
2020-04-20 21:58:41 +0530

    [avani]Add. should return the price one point nine eight of two apple .

--- src/main/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCart.java
+++ src/main/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCart.java
@@ -1,16 +1,24 @@
 package com.thoughtworks.bootcamp.shoppingCart;
 
+import java.util.ArrayList;
+
 public class ShoppingCart {
     private String apple;
     private float price;
+    private int quantity;
 
-    public ShoppingCart(String apple, float price) {
+    public ShoppingCart(String apple, float price, int quantity) {
         this.apple = apple;
         this.price = price;
+        this.quantity = quantity;
     }
 
     public float totalPrice(float price) {
-        return (float) 0.99;
+        float totalPrice = price * getQuantity();
+        return totalPrice;
     }
 
+    public int getQuantity() {
+        return quantity;
+    }
 }
--- src/test/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCartTest.java
@@ -3,16 +3,27 @@ package com.thoughtworks.bootcamp.shoppingCart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class ShoppingCartTest {
 
     @Test
     void givenEmptyShoppingCart_whenAddOneApple_thenShouldReturnOneApplePrice() {
-        ShoppingCart cart = new ShoppingCart("apple", (float) 0.99);
+        ShoppingCart cart = new ShoppingCart("apple", (float) 0.99, 1);
         float oneApplePrice = (float) 0.99;
 
         Assertions.assertEquals((float) 0.99, cart.totalPrice(oneApplePrice));
 
     }
 
+    @Test
+    void givenShoppingCart_whenAppleQuantityIsTwo_thenShouldReturnPriceOnePointNineEight() {
+
+        ShoppingCart shoppingCart = new ShoppingCart("apple", (float) 0.99, 2);
+
+        float applePrice = (float) 0.99;
+        Assertions.assertEquals((float)1.98, shoppingCart.totalPrice(applePrice));
+
+    }
 
 }
