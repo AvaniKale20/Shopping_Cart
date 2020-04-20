27467719e24fac6c9194024ab1a1fce656dfcd50
Avani Kale <avani.kale@thoughtworks.com>
2020-04-20 19:06:51 +0530

    [avani]Refactore. test case from return one apple to return one apple price.

--- src/test/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/bootcamp/shoppingCart/ShoppingCartTest.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 public class ShoppingCartTest {
 
     @Test
-    void givenEmptyShoppingCart_whenAddOneApple_thenShouldReturnOneApple() {
+    void givenEmptyShoppingCart_whenAddOneApple_thenShouldReturnOneApplePrice() {
         ShoppingCart cart = new ShoppingCart("apple", (float) 0.99);
         float oneApplePrice = (float) 0.99;
 
         Assertions.assertEquals((float) 0.99, cart.totalPrice(oneApplePrice));
 
     }
+
 
 }
